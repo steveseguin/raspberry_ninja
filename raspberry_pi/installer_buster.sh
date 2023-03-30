252e8f089f43d00d7a1bd40055142235273cda3e
Steve Seguin <steve@seguin.email>
2023-03-30 11:17:00 -0400

    Update and rename installer.sh to installer_buster.sh

--- raspberry_pi/installer_buster.sh
+++ raspberry_pi/installer_buster.sh
@@ -107,32 +107,174 @@ sudo ldconfig
 sudo libtoolize
 
 cd ~
-git clone --depth 1 https://github.com/mstorsjo/fdk-aac.git
-cd fdk-aac 
-autoreconf -fiv 
-./configure 
-make -j4 
-sudo make install
+git clone --depth 1 https://github.com/mstorsjo/fdk-aac.git ~/ffmpeg-libraries/fdk-aac \
+  && cd ~/ffmpeg-libraries/fdk-aac \
+  && autoreconf -fiv \
+  && ./configure \
+  && make -j$(nproc) \
+  && sudo make install
+sudo ldconfig
+sudo libtoolize
 
+#AV1
+cd ~
+git clone --depth 1 https://code.videolan.org/videolan/dav1d.git ~/ffmpeg-libraries/dav1d \
+  && mkdir ~/ffmpeg-libraries/dav1d/build \
+  && cd ~/ffmpeg-libraries/dav1d/build \
+  && meson .. \
+  && ninja \
+  && sudo ninja install
+sudo ldconfig
+sudo libtoolize
+
+#HEVC
+cd ~
+git clone --depth 1 https://github.com/ultravideo/kvazaar.git ~/ffmpeg-libraries/kvazaar \
+  && cd ~/ffmpeg-libraries/kvazaar \
+  && ./autogen.sh \
+  && ./configure \
+  && make -j$(nproc) \
+  && sudo make install
+
+#AP1
+cd ~
+git clone --depth 1 https://aomedia.googlesource.com/aom ~/ffmpeg-libraries/aom \
+  && mkdir ~/ffmpeg-libraries/aom/aom_build \
+  && cd ~/ffmpeg-libraries/aom/aom_build \
+  && cmake -G "Unix Makefiles" AOM_SRC -DENABLE_NASM=on -DPYTHON_EXECUTABLE="$(which python3)" -DCMAKE_C_FLAGS="-mfpu=vfp -mfloat-abi=hard" .. \
+  && sed -i 's/ENABLE_NEON:BOOL=ON/ENABLE_NEON:BOOL=OFF/' CMakeCache.txt \
+  && make -j$(nproc) \
+  && sudo make install
+sudo ldconfig
+sudo libtoolize
+
+#zimg
+cd ~
+git clone --recursive https://github.com/sekrit-twc/zimg.git ~/ffmpeg-libraries/zimg \
+  && cd ~/ffmpeg-libraries/zimg \
+  && sh autogen.sh \
+  && ./configure \
+  && make \
+  && sudo make install
+sudo ldconfig
+sudo libtoolize
 
 sudo apt-get install libdrm-dev libgmp-dev -y
+sudo apt-get -y install \
+    autoconf \
+    automake \
+    build-essential \
+    cmake \
+    doxygen \
+    git \
+    graphviz \
+    imagemagick \
+    libasound2-dev \
+    libass-dev \
+    libavcodec-dev \
+    libavdevice-dev \
+    libavfilter-dev \
+    libavformat-dev \
+    libavutil-dev \
+    libfreetype6-dev \
+    libgmp-dev \
+    libmp3lame-dev \
+    libopencore-amrnb-dev \
+    libopencore-amrwb-dev \
+    libopus-dev \
+    librtmp-dev \
+    libsdl2-dev \
+    libsdl2-image-dev \
+    libsdl2-mixer-dev \
+    libsdl2-net-dev \
+    libsdl2-ttf-dev \
+    libsnappy-dev \
+    libsoxr-dev \
+    libssh-dev \
+    libssl-dev \
+    libtool \
+    libv4l-dev \
+    libva-dev \
+    libvdpau-dev \
+    libvo-amrwbenc-dev \
+    libvorbis-dev \
+    libwebp-dev \
+    libx264-dev \
+    libx265-dev \
+    libxcb-shape0-dev \
+    libxcb-shm0-dev \
+    libxcb-xfixes0-dev \
+    libxcb1-dev \
+    libxml2-dev \
+    lzma-dev \
+    meson \
+    nasm \
+    pkg-config \
+    python3-dev \
+    python3-pip \
+    texinfo \
+    wget \
+    yasm \
+    libsrt-gnutls-dev \
+    zlib1g-dev
+
+sudo apt-get install libfdk-aac-dev -y
+sudo apt-get install libaom-dev -y
+
 cd ~
 [ ! -d FFmpeg ] && git clone https://github.com/FFmpeg/FFmpeg.git
 cd FFmpeg
 git pull
 make distclean 
-sudo ./configure --extra-cflags="-I/usr/local/include" --extra-ldflags="-L/usr/local/lib" --enable-libopencore-amrnb  --enable-librtmp --enable-libopus --enable-libopencore-amrwb --enable-gmp --enable-version3 --enable-libdrm --enable-shared  --enable-pic --enable-libvpx --enable-libfdk-aac --target-os=linux --enable-gpl --enable-omx --enable-omx-rpi --enable-pthreads --enable-hardcoded-tables  --enable-omx --enable-nonfree --enable-libfreetype --enable-libx264 --enable-mmal --enable-indev=alsa --enable-outdev=alsa --disable-vdpau --extra-ldflags="-latomic"
-libtoolize
+sudo ./configure \
+    --extra-cflags="-I/usr/local/include" \
+    --extra-ldflags="-L/usr/local/lib" \
+    --extra-libs="-lpthread -lm -latomic" \
+    --enable-version3 \
+    --disable-vdpau \
+    --arch=arm64 \
+    --enable-gmp \
+    --enable-gpl \
+    --enable-pic \
+    --enable-shared \
+    --enable-libass \
+    --enable-libdav1d \
+    --enable-indev=alsa \
+    --enable-outdev=alsa \
+    --enable-libfdk-aac \
+    --enable-libfreetype \
+    --enable-libkvazaar \
+    --enable-libmp3lame \
+    --enable-libopencore-amrnb \
+    --enable-libopencore-amrwb \
+    --enable-libopus \
+    --enable-librtmp \
+    --enable-libsnappy \
+    --enable-libsoxr \
+    --enable-libssh \
+    --enable-libvorbis \
+    --enable-libvpx \
+    --enable-libzimg \
+    --enable-libwebp \
+    --enable-libx264 \
+    --enable-libx265 \
+    --enable-libxml2 \
+    --enable-nonfree \
+    --target-os=linux \
+    --enable-pthreads \
+    --enable-openssl \
+    --enable-hardcoded-tables 
+   
 make -j4
 libtoolize
 sudo make install -j4
 sudo ldconfig
 
 cd ~
-wget https://download.gnome.org/sources/glib/2.72/glib-2.72.2.tar.xz
-sudo rm glib-2.72.2 -r || true
-tar -xvf glib-2.72.2.tar.xz
-cd glib-2.72.2
+wget https://download.gnome.org/sources/glib/2.76/glib-2.76.1.tar.xz
+sudo rm glib-2.76.1 -r || true
+tar -xvf glib-2.76.1.tar.xz
+cd glib-2.76.1
 mkdir build
 cd build
 meson --prefix=/usr/local -Dman=false ..
@@ -153,10 +295,10 @@ sudo libtoolize
 
 
 cd ~
-wget https://download.gnome.org/sources/gobject-introspection/1.72/gobject-introspection-1.72.0.tar.xz
-sudo rm  gobject-introspection-1.72.0 -r || true
-tar -xvf gobject-introspection-1.72.0.tar.xz
-cd gobject-introspection-1.72.0
+wget https://download.gnome.org/sources/gobject-introspection/1.76/gobject-introspection-1.76.1.tar.xz
+sudo rm  gobject-introspection-1.76.1 -r || true
+tar -xvf gobject-introspection-1.76.1.tar.xz
+cd gobject-introspection-1.76.1
 mkdir build
 cd build
 sudo meson --prefix=/usr/local --buildtype=release  ..
