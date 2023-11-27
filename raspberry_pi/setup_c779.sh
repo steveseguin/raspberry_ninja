2474bbda255aef89abb2d08484e304e374b82710
Steve Seguin <steve@seguin.email>
2023-11-27 11:03:28 -0500

    Update setup_c779.sh

--- raspberry_pi/setup_c779.sh
+++ raspberry_pi/setup_c779.sh
@@ -25,13 +25,14 @@ sudo raspi-config
 ## REBOOT
 sudo reboot
 
-## you can make your own EDID file; 1080P25 / 1080P30 and even in some cases 1080P60 are possible
+## you can make your own EDID file; 1080P25 / 1080P30 and even in some cases 1080P60 are possible (but not all!)
 wget https://raw.githubusercontent.com/steveseguin/CSI2_device_config/master/1080P50EDID.txt
 v4l2-ctl --set-edid=file=1080P50EDID.txt # load it
 ## If you get an error at this step, check the community forum here: https://forums.raspberrypi.com//viewtopic.php?f=38&t=281972
 # https://raw.githubusercontent.com/steveseguin/CSI2_device_config/master/1080P60EDID.txt
 # v4l2-ctl --set-edid=file=1080P60EDID.txt ## Only if using a RPi Compute Module, since a basic rpi lacks enough data lanes
-## PLUG IN HDMI PLUG NOW
+
+## PLUG IN HDMI NOW - and make sure the source (camera or whatever) can support the resolution and frame rate you specified in the EDID
 v4l2-ctl --set-dv-bt-timings query
 # v4l2-ctl --log-status
 v4l2-ctl --query-dv-timings
