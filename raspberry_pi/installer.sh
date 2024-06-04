fde9dbe86d02496369a45d0a19325a6476901a62
Steve Seguin <steve@seguin.email>
2024-06-04 03:37:26 -0400

    Update installer.sh

--- raspberry_pi/installer.sh
+++ raspberry_pi/installer.sh
@@ -42,6 +42,7 @@ sudo pip3 install scikit-build
 sudo pip3 install ninja 
 sudo pip3 install websockets
 sudo pip3 install python-rtmidi
+pip3 install cryptography
 
 sudo apt-get install apt-transport-https ca-certificates -y
 
